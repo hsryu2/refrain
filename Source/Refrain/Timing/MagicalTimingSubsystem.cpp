f74ec73bee6e6f7e471279a7d0cc6c35d94e2769
one62 <onemo@kakao.com>
2026-06-15 23:52:30 +0900

    음악 재생 테스트

--- Source/Refrain/Timing/MagicalTimingSubsystem.cpp
+++ Source/Refrain/Timing/MagicalTimingSubsystem.cpp
@@ -3,20 +3,165 @@
 
 #include "MagicalTimingSubsystem.h"
 
+#include "Components/AudioComponent.h"
+#include "Engine/AssetManager.h"
+#include "Engine/StreamableManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "Quartz/AudioMixerClockHandle.h"
+#include "Quartz/QuartzSubsystem.h"
 #include "Refrain/Refrain.h"
+#include "Sound/SoundBase.h"
 
 UMagicalTimingSubsystem::UMagicalTimingSubsystem()
 {
 }
 
-void UMagicalTimingSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+bool UMagicalTimingSubsystem::DoesSupportWorldType(const EWorldType::Type WorldType) const
 {
-	Super::Initialize(Collection);
-	RA_LOG(LogRefrain, Log, TEXT("MagicalTimingSubsystem Initialize"));
+	// return Super::DoesSupportWorldType(WorldType);
+	return WorldType == EWorldType::Game || WorldType == EWorldType::PIE;
 }
 
-void UMagicalTimingSubsystem::Deinitialize()
+bool UMagicalTimingSubsystem::SetMusicData(UMagicalMusicData* NewMusicData)
 {
-	Super::Deinitialize();
-	RA_LOG(LogRefrain, Log, TEXT("MagicalTimingSubsystem Deinitialize"));
+	if (!IsValid(NewMusicData))
+	{
+		RA_LOG(LogRefrain, Error, TEXT("MusicData is invalid"));
+		return false;
+	}
+	
+	// MusicData 설정
+	MusicData = NewMusicData;
+	
+	// 기존 로드 취소
+	if (MusicSoundLoadHandle.IsValid())
+	{
+		MusicSoundLoadHandle->CancelHandle();
+	}
+	MusicSoundLoadHandle.Reset();
+
+	if (MusicData->MusicSound.IsNull())
+	{
+		RA_LOG(LogRefrain, Error, TEXT("MusicSound is not set"));
+		return false;
+	}
+
+	// 이미 로드되어 있는 경우
+	if (MusicData->MusicSound.IsValid())
+	{
+		return true;
+	}
+
+	// 비동기 로드
+	FStreamableManager& StreamableManager = UAssetManager::GetStreamableManager();
+	MusicSoundLoadHandle = StreamableManager.RequestAsyncLoad(MusicData->MusicSound.ToSoftObjectPath());
+	return MusicSoundLoadHandle.IsValid();
+}
+
+bool UMagicalTimingSubsystem::StartMusic()
+{
+	if (!IsValid(MusicData))
+	{
+		RA_LOG(LogRefrain, Error, TEXT("MusicData is invalid"));
+		return false;
+	}
+	
+	// 기존 핸들 초기화
+	StopMusic();
+
+	// 음악 사운드
+	USoundBase* MusicSound = MusicData->MusicSound.Get();
+	if (!IsValid(MusicSound))
+	{
+		MusicSound = MusicData->MusicSound.LoadSynchronous();
+	}
+	if (!IsValid(MusicSound))
+	{
+		RA_LOG(LogRefrain, Error, TEXT("MusicSound is invalid"));
+		return false;
+	}
+	
+	// Clock 생성
+	if (!CreateQuartzClock())
+	{
+		RA_LOG(LogRefrain, Error, TEXT("Failed to create Quartz clock"));
+		return false;
+	}
+
+	// AudioComponent 생성
+	AudioComponent = UGameplayStatics::CreateSound2D(
+		GetWorld(), MusicSound, MusicVolume, 1.f, 0.f, nullptr, false, false);
+	if (!IsValid(AudioComponent))
+	{
+		RA_LOG(LogRefrain, Error, TEXT("Failed to create audio component"));
+		return false;
+	}
+	
+	// 재생
+	FQuartzQuantizationBoundary QuantizationBoundary(EQuartzCommandQuantization::Bar);
+	UQuartzClockHandle* RawClockHandle = MusicClockHandle.Get();
+	AudioComponent->PlayQuantized(
+		GetWorld(),
+		RawClockHandle,
+		QuantizationBoundary,
+		FOnQuartzCommandEventBP(),
+		MusicData->StartOffset);
+	
+	return IsValid(AudioComponent);
+}
+
+bool UMagicalTimingSubsystem::StopMusic()
+{
+	if (IsValid(AudioComponent))
+	{
+		AudioComponent->Stop();
+		AudioComponent->DestroyComponent();
+		AudioComponent = nullptr;
+	}
+	
+	if (IsValid(MusicClockHandle))
+	{
+		UQuartzClockHandle* RawClockHandle = MusicClockHandle.Get();
+		MusicClockHandle->StopClock(GetWorld(), true, RawClockHandle);
+		MusicClockHandle = nullptr;
+	}
+
+	return true;
+}
+
+bool UMagicalTimingSubsystem::CreateQuartzClock()
+{
+	UWorld* World = GetWorld();
+	if (!World)
+	{
+		return false;
+	}
+
+	UQuartzSubsystem* QuartzSubsystem = UQuartzSubsystem::Get(World);
+	if (!QuartzSubsystem || !QuartzSubsystem->IsQuartzEnabled())
+	{
+		RA_LOG(LogRefrain, Error, TEXT("Quartz subsystem is not available or disabled."));
+		return false;
+	}
+
+	if (QuartzSubsystem->DoesClockExist(this, ClockName))
+	{
+		QuartzSubsystem->DeleteClockByName(this, ClockName);
+	}
+
+	// MusicData 설정 필요
+	MusicClockHandle = QuartzSubsystem->CreateNewClock(
+		this, ClockName, MusicData->ToQuartzClockSettings(), true);
+	if (!MusicClockHandle)
+	{
+		RA_LOG(LogRefrain, Error, TEXT("Failed to create Quartz clock: %s"), *ClockName.ToString());
+		return false;
+	}
+	UQuartzClockHandle* RawClockHandle = MusicClockHandle.Get();
+	MusicClockHandle->SetBeatsPerMinute(
+		World, FQuartzQuantizationBoundary(), FOnQuartzCommandEventBP(), RawClockHandle, MusicData->BPM);
+	
+	MusicClockHandle->StartClock(World, RawClockHandle);
+
+	return true;
 }
--- Source/Refrain/Timing/MagicalTimingSubsystem.h
+++ Source/Refrain/Timing/MagicalTimingSubsystem.h
@@ -3,9 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "MagicalMusicData.h"
 #include "Subsystems/WorldSubsystem.h"
 #include "MagicalTimingSubsystem.generated.h"
 
+class UAudioComponent;
+class UQuartzClockHandle;
+struct FStreamableHandle;
 /**
  * 
  */
@@ -17,13 +21,45 @@ class REFRAIN_API UMagicalTimingSubsystem : public UWorldSubsystem
 public:
 	UMagicalTimingSubsystem();
 	
-	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-	virtual void Deinitialize() override;
+// 엔진 재정의 함수
+protected:
+	virtual bool DoesSupportWorldType(const EWorldType::Type WorldType) const override;
+	
+	
+public:
+// 게임 로직 - 음악 재생
+	UFUNCTION(BlueprintCallable, Category = Music)
+	bool SetMusicData(UMagicalMusicData* NewMusicData);
+	
+	UFUNCTION(BlueprintCallable, Category = Music)
+	bool StartMusic();
+	
+	UFUNCTION(BlueprintCallable, Category = Music)
+	bool StopMusic();
+	
+// 
 	
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Music, meta=(ClampMin=60, ClampMax=240))
-	float MusicBPM;
+// 재생 중인 음악 관련 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Music)
+	TObjectPtr<UMagicalMusicData> MusicData;
+
+	TSharedPtr<FStreamableHandle> MusicSoundLoadHandle;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Music)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Music)
+	float MusicVolume = 1.f;
+	
+	
+private:
+// Quartz 시스템
+	bool CreateQuartzClock();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Music, meta = (AllowPrivateAccess = true))
 	FName ClockName = TEXT("MusicClock");
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UQuartzClockHandle> MusicClockHandle;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UAudioComponent> AudioComponent;
 };
