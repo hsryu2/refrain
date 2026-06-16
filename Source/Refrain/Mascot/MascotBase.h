ad08f8a1ea73f0f923f693ccc09774a7394f6ac6
one62 <onemo@kakao.com>
2026-06-16 12:35:08 +0900

    마스코트 클래스 추가

---
+++ Source/Refrain/Mascot/MascotBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "MascotBase.generated.h"
+
+UCLASS()
+class REFRAIN_API AMascotBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMascotBase();
+	
+};
