25b5cadc1ba9d96b149ac324f38046236696d45a
one62 <onemo@kakao.com>
2026-07-02 01:34:18 +0900

    함수명 변경

--- Source/Refrain/Timing/MagicalTimingSubsystem.h
+++ Source/Refrain/Timing/MagicalTimingSubsystem.h
@@ -79,7 +79,7 @@ public:
 	
 	// 다음 타격 타이밍까지 남은 시간을 반환하는 함수 (공격 모션 재생용)
 	UFUNCTION(BlueprintCallable, Category = "Magical|Timing", meta = (CPP_Default_Quantization = "Beat"))
-	float GetTimeUntilNextHit(float MinimumStartupDelay, EQuartzCommandQuantization TargetQuantization = EQuartzCommandQuantization::Beat, float Multiplier = 1.f);
+	float GetTimeUntilNextBeat(float MinimumStartupDelay, EQuartzCommandQuantization TargetQuantization = EQuartzCommandQuantization::Beat, float Multiplier = 1.f);
 
 // 내부 로직에 필요한 함수
 private:
