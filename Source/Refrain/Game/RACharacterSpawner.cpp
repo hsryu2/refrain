f795efb2138444f3b6573eb064ec766cd8d9532a
one62 <onemo@kakao.com>
2026-07-10 16:47:15 +0900

    머티리얼 적용 안되어있던 문제 수정

--- Source/Refrain/Game/RACharacterSpawner.cpp
+++ Source/Refrain/Game/RACharacterSpawner.cpp
@@ -2,7 +2,10 @@
 
 
 #include "RACharacterSpawner.h"
+
+#include "TimerManager.h"
 #include "Character/RACharacterNonPlayer.h"
+#include "Engine/World.h"
 
 // Sets default values
 ARACharacterSpawner::ARACharacterSpawner()
